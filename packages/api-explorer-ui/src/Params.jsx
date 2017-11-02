7b35b85a81f9b79eae8c57f0add4f06f87b5d83e
Dom Harrington <dom@harrington-mail.com>
2017-11-02 10:53:25 -0700

    Committing slightly more progress but gunna move onto something else for now
    This is still broken and shouldn't be merged. Basically we need to move the onSubmit logic up to the Doc component,
    then anywhere onSubmit is called (on enter press in any of the forms or click of the try it now button), it needs
    to validate the forms, if they arent valid it should trigger a click on the form's submit button to display the
    browser's native validation overlays then when they're valid, it should carry on and do the isAuthReady check

--- packages/api-explorer-ui/src/Params.jsx
+++ packages/api-explorer-ui/src/Params.jsx
@@ -14,67 +14,96 @@ const Oas = require('./lib/Oas');
 const { Operation } = Oas;
 const parametersToJsonSchema = require('./lib/parameters-to-json-schema');
 
-function Params({ oas, operation, formData, onChange, onSubmit }) {
-  const jsonSchema = parametersToJsonSchema(operation, oas);
+class Params extends React.Component {
 
-  const forms = [];
+  render() {
+    const { oas, operation, formData, onChange, onSubmit } = this.props;
+    const jsonSchema = parametersToJsonSchema(operation, oas);
 
-  // This is kinda hacky but it's the only way we can make sure we
-  // only call onSubmit when all of the forms are valid
-  //
-  // Otherwise if the users presses <enter> whilst they're in the first
-  // form, it'll bypass any validation in the 2nd form.
-  function submit() {
-    return forms.every(form => {
-      return form.validate(form.state.formData).errors.length === 0;
-    }) && onSubmit();
-  }
+    const forms = [];
+    const submitButtons = [];
 
-  return (
-    <div className="api-manager">
-      <div className="param-table">
-        {jsonSchema &&
-          jsonSchema.map((schema) => {
-            return [
-              <div className="param-header" key={`${schema.type}-header`}>
-                <h3>{schema.label}</h3>
-                <div className="param-header-border" />
-              </div>,
-              <Form
-                key={`${schema.type}-form`}
-                id={`form-${operation.operationId}-${schema.type}`}
-                schema={schema.schema}
-                widgets={{
-                  int64: UpDownWidget,
-                  int32: UpDownWidget,
-                  double: UpDownWidget,
-                  float: UpDownWidget,
-                  binary: FileWidget,
-                  TextWidget,
-                }}
-                onSubmit={submit}
-                formData={formData[schema.type]}
-                onChange={form => {
-                  // return onChange({ [schema.type]: { $set: form.formData } })
-                  return onChange({ [schema.type]: form.formData });
-                }}
-                onError={() => {}}
-                FieldTemplate={FieldTemplate}
-                fields={{
-                  ObjectField,
-                  SchemaField,
-                  TitleField: () => null,
-                }}
-                ref={form => forms.push(form)}
-              >
-                <button type="submit" style={{ display: 'none' }} />
-              </Form>,
-            ];
-          })}
+    console.log('render');
+
+    // This is kinda hacky but it's the only way we can make sure we
+    // only call onSubmit when all of the forms are valid
+    //
+    // Otherwise if the users presses <enter> whilst they're in the first
+    // form, it'll bypass any validation in the 2nd form.
+    function submit() {
+      console.log(submitButtons);
+      return (
+        forms.every((form, i) => {
+          const isValid = form.validate(form.state.formData).errors.length === 0;
+          if (!isValid) {
+            console.log(submitButtons, submitButtons.length, submitButtons[i]);
+            submitButtons[i].click();
+            //   form.onSubmit({ preventDefault() {} });
+          }
+          return isValid;
+        }) && onSubmit()
+      );
+    }
+
+    return (
+      <div className="api-manager">
+        <div className="param-table">
+          {jsonSchema &&
+            jsonSchema.map(schema => {
+              console.log('rendering new form');
+              return [
+                <div className="param-header" key={`${schema.type}-header`}>
+                  <h3>{schema.label}</h3>
+                  <div className="param-header-border" />
+                </div>,
+                <Form
+                  key={`${schema.type}-form`}
+                  id={`form-${operation.operationId}-${schema.type}`}
+                  schema={schema.schema}
+                  widgets={{
+                    int64: UpDownWidget,
+                    int32: UpDownWidget,
+                    double: UpDownWidget,
+                    float: UpDownWidget,
+                    binary: FileWidget,
+                    TextWidget,
+                  }}
+                  onSubmit={submit}
+                  formData={formData[schema.type]}
+                  onChange={form => {
+                    // return onChange({ [schema.type]: { $set: form.formData } })
+                    return onChange({ [schema.type]: form.formData });
+                  }}
+                  onError={() => {}}
+                  FieldTemplate={FieldTemplate}
+                  fields={{
+                    ObjectField,
+                    SchemaField,
+                    TitleField: () => null,
+                  }}
+                  ref={form => forms.push(form)}
+                >
+                  <button
+                    ref={submitButton => {
+                      console.log(submitButton);
+                      submitButton && submitButtons.push(submitButton);
+                      console.log(11111, submitButtons);
+                    }}
+                    type="submit"
+                    style={{ display: 'none' }}
+                  />
+                </Form>,
+              ];
+            })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+// function Params() {
+
+// }
 
 Params.propTypes = {
   oas: PropTypes.instanceOf(Oas).isRequired,
